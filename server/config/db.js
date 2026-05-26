438e96a304a8e43c369d5969a0d2646c1227f465
Tom Augustin Joseph <tompjoseph@gmail.com>
2026-05-26 21:42:11 +0530

    Don't crash server on MongoDB connection failure

--- server/config/db.js
+++ server/config/db.js
@@ -6,7 +6,6 @@ const connectDB = async () => {
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`MongoDB connection error: ${error.message}`);
-    process.exit(1);
   }
 };
 
